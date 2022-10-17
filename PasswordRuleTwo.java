0cbeba86ce3ac2b5c1b195946aba9b49459650de
Madhu Sudhan <m02sudhan@gmail.com>
2022-10-17 09:19:46 +0530

    Added : rule two for password

---
+++ PasswordRuleTwo.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRuleTwo {
+    public static void main(String[] args) {
+
+        Pattern pattern= Pattern.compile("^[a-zA-Z]{8,}+$") ;
+        Matcher matcher = pattern.matcher("MadhuSudhan");
+
+        boolean found = matcher.find();
+        System.out.println("found : " + found);
+    }
+}
